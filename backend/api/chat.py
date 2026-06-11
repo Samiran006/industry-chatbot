a31aacf1033793c3a28f9695d8c858e06f18e0a6
Samiran Bangal <samiranbangal0@gmail.com>
2026-06-11 21:31:44 +0530

    Phase 1 FastAPI setup completed

---
+++ backend/api/chat.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from backend.services.llm_service import ask_llm
+
+router = APIRouter()
+
+@router.get("/chat")
+def chat(query: str):
+
+    answer = ask_llm(query)
+
+    return {
+        "response": answer
+    }
---
+++ backend/services/llm_service.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    model_name="llama-3.3-70b-versatile",
+    temperature=0.3
+)
+
+def ask_llm(question):
+
+    response = llm.invoke(question)
+
+    return response.content
